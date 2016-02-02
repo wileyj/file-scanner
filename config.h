e9ea6e625a1dc413959fdd4cfe294bb2b28d713a
Jesse Wiley <wileyj@Jesses-MacBook-Air.local>
2016-02-02 12:03:21 -0500

    updating email

--- config.h
+++ config.h
@@ -95,7 +95,7 @@
 #define PACKAGE "filescanner"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "jesse.wiley@viacom.com"
+#define PACKAGE_BUGREPORT "user@local.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "filescanner"
